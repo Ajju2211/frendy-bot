9954821befad0c22ef39f00a71f15270132b99f2
Ajju2211 <azharuddinm2211@gmail.com>
2020-09-13 19:36:53 +0530

    deleted extra file

--- action-services.sh
+++
@@ -1,5 +0,0 @@
-cd app/
-# Start rasa actions server 
-# rasa run actions --actions app.actions --enable-api --cors "*" --debug \
-#          -p $PORT
-rasa run actions --actions actionserver.actions  --cors "*" --debug -p $PORT

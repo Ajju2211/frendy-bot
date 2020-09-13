f20fee6a63561ffbc5dd8c67c4ba901da5933eb2
Ajju2211 <azharuddinm2211@gmail.com>
2020-09-13 19:19:45 +0530

    Added action-server docker

--- start_services.sh
+++ start_services.sh
@@ -5,5 +5,5 @@ cd app/
 #         --endpoints endpoints.yml \
 #         --credentials credentials.yml \
 #         -p $PORT
-rasa run --model models --enable-api --cors "*" --debug \
+rasa run --model models  --endpoints server-endpoints.yml --enable-api --cors "*" --debug \
          -p $PORT

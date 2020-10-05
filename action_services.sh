e4ed2f98b29ca2517a22fe3168ab0fb2984503c8
N-Harish <48133371+N-Harish@users.noreply.github.com>
2020-10-05 21:39:39 +0530

    Update action_services.sh

--- action_services.sh
+++ action_services.sh
@@ -1,4 +1,5 @@
 # Start rasa actions server 
 # rasa run actions --actions app.actions --enable-api --cors "*" --debug \
 #          -p $PORT
+cd app/
 rasa run actions --actions actionserver.actions  --cors "*" --debug -p $PORT
